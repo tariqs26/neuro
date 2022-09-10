6f129c245d5937e97f986984608a96a0cb6b1f6f
Saad T <tariqs26@mcmaster.ca>
2022-09-09 23:47:37 -0400

    take code to fetch questions from quiz component, to a custom hook

---
+++ src/hooks/useQuestionsFetch.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateQuestions, updateIsLoading } from "features/quizSlice";
+import getQuestionsProxy from "api/getQuestionsProxy";
+
+export const useQuestionsFetch = (id, location) => {
+  const dispatch = useDispatch();
+  const { questions } = useSelector((state) => state.quiz);
+  useEffect(() => {
+    const fetchData = async () => {
+      let data;
+      if (!id && !questions.length) {
+        data = await getQuestionsProxy(location.state.url);
+        data = data.map((question) => ({
+          ...question,
+          picked: "",
+          questions: [
+            question.correct_answer,
+            ...question.incorrect_answers,
+          ].sort(() => Math.random() - 0.5),
+        }));
+      } else if (id) data = [];
+      questions.length || dispatch(updateQuestions(data));
+      dispatch(updateIsLoading(false));
+    };
+    fetchData();
+  }, [dispatch, id, location, questions]);
+};
