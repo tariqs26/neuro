8dd9b8d886a77eafd25e8e36a4c97ffa05298ef0
Saad T <tariqs26@mcmaster.ca>
2022-09-09 21:23:10 -0400

    cleanup function for modal

--- src/components/Quiz/QuizModal/QuizModal.js
+++ src/components/Quiz/QuizModal/QuizModal.js
@@ -12,6 +12,9 @@ export default function QuizModal() {
 
   useEffect(() => {
     document.body.style.overflow = isOpen ? "hidden" : "unset";
+    return () => {
+      document.body.style.overflow = "unset";
+    }
   }, [isOpen]);
 
   return (
