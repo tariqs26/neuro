fce5f55dde1f62b5da060281c7885bb4e3965bc4
Saad T <tariqs26@mcmaster.ca>
2022-09-09 23:59:49 -0400

    remove logic to fetch questions, make the no questions case into a component, with back button

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,8 +1,6 @@
-import { useParams, useLocation } from "react-router-dom";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { updateQuestions, updateIsLoading } from "features/quizSlice";
-import getQuestionsProxy from "api/getQuestionsProxy";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useQuestionsFetch } from "hooks/useQuestionsFetch";
 import QuizHeader from "./QuizHeader/QuizHeader";
 import Question from "../Question/Question";
 import QuizModal from "./QuizModal/QuizModal";
@@ -11,28 +9,8 @@ import "./Quiz.css";
 export default function Quiz() {
   const location = useLocation();
   const { id } = useParams();
-  const dispatch = useDispatch();
+  useQuestionsFetch(id, location);
   const { isLoading, questions } = useSelector((state) => state.quiz);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      let data;
-      if (!id && !questions.length) {
-        data = await getQuestionsProxy(location.state.url);
-        data = data.map((question) => ({
-          ...question,
-          picked: "",
-          questions: [
-            question.correct_answer,
-            ...question.incorrect_answers,
-          ].sort(() => Math.random() - 0.5),
-        }));
-      } else if (id) data = [];
-      questions.length || dispatch(updateQuestions(data));
-      dispatch(updateIsLoading(false));
-    };
-    fetchData();
-  }, [dispatch, id, location, questions]);
 
   return (
     <div className="quiz">
@@ -40,7 +18,7 @@ export default function Quiz() {
       {isLoading ? (
         <h1 className="loader">Loading...</h1>
       ) : !(questions && questions.length) ? (
-        <h1 className="loader">No Questions Found</h1>
+        <NoQuestions />
       ) : (
         <>
           <QuizHeader />
@@ -54,3 +32,15 @@ export default function Quiz() {
     </div>
   );
 }
+
+const NoQuestions = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="no-questions">
+      <h1 className="loader">No Questions Found</h1>
+      <button className="back" onClick={() => navigate("/")}>
+        back
+      </button>
+    </div>
+  );
+};
