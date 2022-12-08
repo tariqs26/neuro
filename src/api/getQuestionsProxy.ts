55d5ad7bc48880dd1b69764c435e18e6b2639e0f
Saad T <tariqs26@mcmaster.ca>
2022-12-08 18:01:14 -0500

    arrow function syntax

--- src/api/getQuestionsProxy.ts
+++ src/api/getQuestionsProxy.ts
@@ -2,7 +2,9 @@ import get from 'axios';
 import { FormState } from 'features/formSlice';
 
 const API_URL = 'https://opentdb.com/api.php';
-export default async function getQuestionsProxy(params: FormState) {
+const getQuestionsProxy = async (params: FormState) => {
   const { results } = (await get(API_URL, { params })).data;
   return results;
-}
+};
+
+export default getQuestionsProxy;
