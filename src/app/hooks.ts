1ef51162d5f585b5c09bee6c55db432b828eac95
Saad T <tariqs26@mcmaster.ca>
2022-12-11 00:01:06 -0500

    import with relative path, bundle exports together

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,7 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { RootState, AppDispatch } from 'app/store';
+import { RootState, AppDispatch } from './store';
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+const useAppDispatch = () => useDispatch<AppDispatch>(),
+  useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export { useAppDispatch, useAppSelector };
