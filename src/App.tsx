2e54d7cb2b6ae2091d2d74808c9fef2af2d6d5f6
Saad T <tariqs26@mcmaster.ca>
2022-12-16 17:55:12 -0500

    add div with className App, with a page container to contain the pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import Form from 'components/Form/Form';
 import Quiz from 'components/Quiz/Quiz';
 import Navbar from 'components/Navbar/Navbar';
 import Results from 'components/Results/Results';
+import './App.css';
 
 export default function App() {
   const { page } = useAppSelector((state) => state.app);
   return (
-    <>
+    <div className='App'>
       <Navbar />
-      {page === 'home' && <Form />}
-      {page === 'quiz' && <Quiz />}
-      {page === 'results' && <Results />}
-    </>
+      <div className='page-container'>
+        {page === 'home' && <Form />}
+        {page === 'quiz' && <Quiz />}
+        {page === 'results' && <Results />}
+      </div>
+    </div>
   );
 }
