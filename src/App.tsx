b118cf2cc0bf538d4726615c1aa80e5e1dc25c55
Saad T <tariqs26@mcmaster.ca>
2022-12-23 00:42:52 -0500

    add leave button logic directly in the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import Form from 'components/Form/Form';
 import Quiz from 'components/Quiz/Quiz';
 import Navbar from 'components/Navbar/Navbar';
 import Results from 'components/Results/Results';
+import LeaveButton from 'components/LeaveButton/LeaveButton';
 import './App.css';
 
 export default function App() {
   const { page } = useAppSelector((state) => state.app);
   return (
     <div className='App'>
-      <Navbar />
+      <Navbar>{page === 'quiz' && <LeaveButton />}</Navbar>
       <div className='page-container'>
         {page === 'home' && <Form />}
         {page === 'quiz' && <Quiz />}
