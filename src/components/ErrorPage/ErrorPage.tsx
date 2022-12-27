a1cfc92a910e80e23ff90466581d6600ee6b577a
Saad T <tariqs26@mcmaster.ca>
2022-12-27 18:31:15 -0500

    new error page component

---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,17 @@
+import './ErrorPage.css';
+
+const ErrorPage = () => {
+  return (
+    <div className='error-page page'>
+      <h1 className='loader'>No Questions Found</h1>
+      <p>
+        Sorry, there are no questions available for the selected options. This
+        is most likely due to the selected category not having enough questions,
+        or questions of the selected difficulty or type. Please try again with
+        different options.
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
