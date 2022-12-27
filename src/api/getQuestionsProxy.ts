1b321f18aa71d573df1a70f2a00b9fdb76ebcf50
Saad T <tariqs26@mcmaster.ca>
2022-12-27 17:21:39 -0500

    add timeout to api call

--- src/api/getQuestionsProxy.ts
+++ src/api/getQuestionsProxy.ts
@@ -3,7 +3,7 @@ import { FormState } from 'features/formSlice';
 
 const API_URL = 'https://opentdb.com/api.php';
 const getQuestionsProxy = async (params: FormState) => {
-  const { results } = (await get(API_URL, { params })).data;
+  const { results } = (await get(API_URL, { params, timeout: 5000 })).data;
   return results;
 };
 
