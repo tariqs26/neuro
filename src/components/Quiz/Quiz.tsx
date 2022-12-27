605db151e68069d0474f29c4e4d09ba533d7c7b0
Saad T <tariqs26@mcmaster.ca>
2022-12-27 17:42:43 -0500

    conistent width for modals, update message for error modal

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -42,6 +42,12 @@ export default function Quiz() {
   ) : isError ? (
     <div className='no-questions page'>
       <h1 className='loader'>No Questions Found</h1>
+      <p>
+        Sorry, there are no questions available for the selected options. This
+        is most likely due to the selected category not having enough questions,
+        or questions of the selected difficulty or type. Please try again with
+        different options.
+      </p>
       <button onClick={() => dispatch(setPage('home'))}>home</button>
     </div>
   ) : (
