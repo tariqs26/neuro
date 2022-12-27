7442bb5842dfe52c162acbfb0889c0aa26643eef
Saad T <tariqs26@mcmaster.ca>
2022-12-27 17:32:49 -0500

    move icon to Icons file

--- src/components/Quiz/QuizFooter/components/Timer.tsx
+++ src/components/Quiz/QuizFooter/components/Timer.tsx
@@ -1,5 +1,6 @@
 import { useAppSelector } from 'app/hooks';
 import { useTimer } from 'hooks/useTimer';
+import { TimerIcon } from 'components/Icons';
 
 export default function Timer() {
   const { isTimerStopped, isTimerComplete, timeElapsed } = useAppSelector(
@@ -29,20 +30,3 @@ export default function Timer() {
     </h3>
   );
 }
-
-const TimerIcon = () => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    width='24'
-    height='24'
-    viewBox='0 0 24 24'
-    fill='none'
-    stroke='currentColor'
-    strokeWidth='2'
-    strokeLinecap='round'
-    strokeLinejoin='round'
-  >
-    <circle cx='12' cy='12' r='10'></circle>
-    <polyline points='12 6 12 12 16 14'></polyline>
-  </svg>
-);
