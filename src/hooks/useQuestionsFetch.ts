c6a9f6ea115a0397f734ee7415d2ff519afc70d9
Saad Tariq <tariqs26@mcmaster.ca>
2023-07-06 17:12:41 -0400

    types declaration

--- src/hooks/useQuestionsFetch.ts
+++ src/hooks/useQuestionsFetch.ts
@@ -1,39 +1,31 @@
-import { useEffect } from 'react';
-import { useAppSelector, useAppDispatch } from 'app/hooks';
-import { setPage } from 'features/appSlice';
-import { setQuestions, setIsLoading } from 'features/quizSlice';
-import getQuestionsProxy from 'api/getQuestionsProxy';
-
-export interface Question {
-  category: string;
-  correct_answer: string;
-  difficulty: 'easy' | 'medium' | 'hard';
-  incorrect_answers: string[];
-  question: string;
-  type: 'multiple' | 'boolean';
-}
+import { useEffect } from "react";
+import { useAppSelector, useAppDispatch } from "app/hooks";
+import { setPage } from "features/appSlice";
+import { setQuestions, setIsLoading } from "features/quizSlice";
+import getQuestionsProxy from "api/getQuestionsProxy";
 
 export const useQuestionsFetch = () => {
   const dispatch = useAppDispatch();
   const params = useAppSelector((state) => state.form);
   useEffect(() => {
     const fetchData = async () => {
-      let data = [];
       try {
         let res = await getQuestionsProxy(params);
-        data = res.map((question: Question) => ({
+        const data = res.map((question: Question) => ({
           ...question,
-          picked: '',
+          picked: "",
           options: [
             question.correct_answer,
             ...question.incorrect_answers,
           ].sort(() => Math.random() - 0.5),
           score: 0,
         }));
-      } catch (err) {}
-      if (!data.length) dispatch(setPage('error'));
-      else dispatch(setQuestions(data));
-      dispatch(setIsLoading(false));
+        dispatch(setQuestions(data));
+      } catch (err) {
+        dispatch(setPage("error"));
+      } finally {
+        dispatch(setIsLoading(false));
+      }
     };
     fetchData();
   }, []);
---
+++ src/types/index.d.ts
@@ -0,0 +1,8 @@
+interface Question {
+  category: string;
+  correct_answer: string;
+  difficulty: "easy" | "medium" | "hard";
+  incorrect_answers: string[];
+  question: string;
+  type: "multiple" | "boolean";
+}
