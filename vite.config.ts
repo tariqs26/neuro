4221b39a2014b5de04a8d55b1606fbd84e59f6a6
Saad Tariq <saadtariq.dev@outlook.com>
2024-06-21 08:58:11 -0400

    refactor: remove port configuration options

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import viteTsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
-  server: {
-    host: 'localhost',
-    port: 3000,
-  },
-});
+})
