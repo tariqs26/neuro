df4a853682bd304594a39d00b33f36a7aeefe7fd
Saad Tariq <saadtariq.dev@outlook.com>
2024-06-21 10:18:35 -0400

    style: extract ternary statements to separate variables for better readability

--- src/pages/results/components/Title.tsx
+++ src/pages/results/components/Title.tsx
@@ -1,21 +1,22 @@
-const Title = ({ percentCorrect }: { percentCorrect: number }) => (
-  <h1
-    className={`${
-      percentCorrect === 1
-        ? "flawless"
-        : percentCorrect > 0.5
-        ? "excellent"
-        : "fail"
-    } results-title`}
-  >
-    {percentCorrect === 1
+const Title = ({ percentCorrect }: { percentCorrect: number }) => {
+  const className = `result-title ${
+    percentCorrect === 1
+      ? "flawless"
+      : percentCorrect > 0.5
+      ? "excellent"
+      : "fail"
+  }`
+
+  const title =
+    percentCorrect === 1
       ? "Flawless!"
       : percentCorrect > 0.7
       ? "Excellent!"
       : percentCorrect > 0.5
       ? "You Passed"
-      : "You Failed"}
-  </h1>
-)
+      : "You Failed"
+
+  return <h1 className={className}>{title}</h1>
+}
 
 export default Title
