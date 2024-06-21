609e829a7f0e3cf7bc367258c0000a11435d508d
Saad Tariq <saadtariq.dev@outlook.com>
2024-06-21 09:09:17 -0400

    refactor: remove unused files

--- src/hooks/useText.ts
+++
@@ -1,9 +0,0 @@
-import { useRef, useEffect } from 'react';
-
-export const useText = <T extends HTMLElement>(text: string) => {
-  const textRef = useRef<T>(null);
-  useEffect(() => {
-    (textRef.current as T).innerHTML = text;
-  }, [text]);
-  return textRef;
-};
