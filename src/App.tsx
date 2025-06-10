8c8b5b6e27da32da36d7cfdfe7db9af86d008b86
Saad Tariq <saadtariq.dev@outlook.com>
2025-06-09 20:03:24 -0400

    refactor: update App component imports and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { useSelector } from "./app/hooks"
 import type { Page } from "./types"
 
-import Navbar from "./components/Navbar"
-import ErrorPage from "./pages/Error"
-import Form from "./pages/form/Form"
-import Quiz from "./pages/quiz/Quiz"
-import Results from "./pages/results/Results"
+import { Navbar } from "./components/Navbar"
+import { ErrorPage } from "./pages/Error"
+import { FormPage } from "./pages/form/Form"
+import { QuizPage } from "./pages/quiz/Quiz"
+import { ResultsPage } from "./pages/results/Results"
 
 import "./App.css"
 
-const pageMap: Record<Page, JSX.Element> = {
-  home: <Form />,
-  quiz: <Quiz />,
-  results: <Results />,
+const pageMap: Record<Page, React.ReactElement> = {
+  home: <FormPage />,
+  quiz: <QuizPage />,
+  results: <ResultsPage />,
   error: <ErrorPage />,
 }
 
-export default function App() {
+export const App = () => {
   const { page } = useSelector((state) => state.app)
 
   return (
