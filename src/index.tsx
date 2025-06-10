6270a7ebda48ae4f1fee3fafe16d434e7b07b975
Saad Tariq <saadtariq.dev@outlook.com>
2025-06-09 20:04:17 -0400

    refactor: change App import to named export

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
-import App from "./App"
+import { App } from "./App"
 import { store } from "./app/store"
+
 import "./index.css"
 
 createRoot(document.querySelector("#root") as HTMLDivElement).render(
