eee7215996b8356867512d99f93211743b3f4ca1
Saad Tariq <saadtariq.dev@outlook.com>
2025-06-10 16:36:35 -0400

    refactor: simplify ErrorPage component by using implicit return

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,13 +1,11 @@
-export const ErrorPage = () => {
-  return (
-    <div className="error-page page">
-      <h1 className="loader">No Questions Found</h1>
-      <p>
-        Sorry, there are no questions available for the selected options. This
-        is most likely due to the selected category not having enough questions,
-        or questions of the selected difficulty or type. Please try again with
-        different options.
-      </p>
-    </div>
-  )
-}
+export const ErrorPage = () => (
+  <div className="error-page page">
+    <h1 className="loader">No Questions Found</h1>
+    <p>
+      Sorry, there are no questions available for the selected options. This is
+      most likely due to the selected category not having enough questions, or
+      questions of the selected difficulty or type. Please try again with
+      different options.
+    </p>
+  </div>
+)
